e5b3fcb896d817d05869cb6107966bd1b7fb21dd
pongpipha-k <pongpiphat_k@cmu.ac.th>
2025-12-19 10:54:16 +0700

    Decorate message box

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,8 +10,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
+    return Scaffold(
+      
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+      child: ListView(
+        children: [
           MyBox(),
           SizedBox(height: 24,),
           MyBox(),
@@ -19,14 +23,39 @@ class _HomePageState extends State<HomePage> {
           MyBox(),
           SizedBox(height: 24,),
       ],
+      )
+      )
     );
   }
 
   Widget MyBox() {
     return Container(
-      child: Column(children: [
-        Text("What is a computer", style: TextStyle(fontSize: 25),),
-        Text("Computer is a things to calculate and for any other works", style: TextStyle(fontSize: 15),)
+      padding: EdgeInsets.all(20),
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.blue[200],
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: NetworkImage("https://cdn.pixabay.com/photo/2023/11/18/18/20/christmas-8396941_1280.png"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken)
+          )
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text("What is a computer", 
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+          ),),
+        Text("Computer is a things to calculate and for any other works", 
+        style: TextStyle(fontSize: 15,
+        color: Colors.white
+        ),)
       ],),
     );
   }
